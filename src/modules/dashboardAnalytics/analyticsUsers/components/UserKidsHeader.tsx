8b2788ed35bd15cc8adddda336c5d06a8a9b1b24
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 17:23:39 -0500

    add: user kids header

---
+++ src/modules/dashboardAnalytics/analyticsUsers/components/UserKidsHeader.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import { Kid } from '../../../../core/types/kids_types';
+
+interface Props {
+  kids: Kid[];
+  customerId: string;
+}
+
+function UserKidsHeader({ kids, customerId }: Props) {
+  const navigate = useNavigate();
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <button className="btn btn-ghost mr-3" onClick={() => navigate(-1)}>
+          Volver
+        </button>
+        <a className="text-xl">
+          Niños del usuario{' '}
+          <span className="badge badge-outline badge-primary">
+            {customerId}
+          </span>
+        </a>
+      </div>
+      <div className="flex-none">
+        <button className="btn  btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline-block mr-1"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1m-4-4l4 4m0 0l-4-4m4 4H8"
+            />
+          </svg>
+          Reporte PDF
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UserKidsHeader;
