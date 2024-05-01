41baf50bcac4fe38ac3a0ea8e7fd19b9802e2f77
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 03:17:28 -0500

    remove unused code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,4 @@
 export { default as AccordionFAQ } from './AccordionFAQ/AccordionFAQ';
-export { default as Cart } from './Cart/Cart';
-export { default as CartItem } from './CartItem/CartItem';
 export { default as Footer } from './Footer/Footer';
 export { default as Navbar } from './Navbar/Navbar';
-export { default as NecessitiesCard } from './NecessitiesCard/NecessitiesCard';
-export { default as ServiceCard } from './ServiceCard/ServiceCard';
 export * from './index';
