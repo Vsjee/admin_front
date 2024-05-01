2dd89e35dec048c9980693189975cc4c2acbe341
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 18:22:50 -0500

    add: basic books detail layout

--- src/modules/dashboardAnalytics/analyticsUsers/pages/UserBooksDetail.tsx
+++ src/modules/dashboardAnalytics/analyticsUsers/pages/UserBooksDetail.tsx
@@ -1,5 +1,33 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import UserBooksTable from '../components/UserBooksTable';
+import booksService from '../../../../core/services/books_types';
+import { IBook } from '../../../../core/types/books_types';
+
 function UserBooksDetail() {
-  return <div>UserBooksDetail</div>;
+  const { id } = useParams();
+  const [books, setBooks] = useState<IBook[]>([]);
+
+  function initStories() {
+    booksService
+      .getBooksByCustomerId(id!)
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    initStories();
+  }, []);
+
+  return (
+    <>
+      <UserBooksTable books={books} />
+    </>
+  );
 }
 
 export default UserBooksDetail;
