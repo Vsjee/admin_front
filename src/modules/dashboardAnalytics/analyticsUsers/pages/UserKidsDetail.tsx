5d7b567d51d489b26b67c062b59fe755f32cdc32
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 15:28:25 -0500

    add: init kids in kids detail

--- src/modules/dashboardAnalytics/analyticsUsers/pages/UserKidsDetail.tsx
+++ src/modules/dashboardAnalytics/analyticsUsers/pages/UserKidsDetail.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import UserKidsTable from '../components/UserKidsTable';
+import { Kid } from '../../../../core/types/kids_types';
+import kidsService from '../../../../core/services/kids_service';
+import { useParams } from 'react-router-dom';
+
 function UserKidsDetail() {
-  return <div>UserKidsDetail</div>;
+  const { id } = useParams();
+  const [kids, setKids] = useState<Kid[]>([]);
+
+  function initKids() {
+    kidsService
+      .getKidsByCustomerId(id!)
+      .then((response) => {
+        setKids(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    initKids();
+  }, []);
+
+  return (
+    <>
+      <UserKidsTable kids={kids} />
+    </>
+  );
 }
 
 export default UserKidsDetail;
