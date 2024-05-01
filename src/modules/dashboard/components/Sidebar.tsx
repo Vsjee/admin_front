1f2c26cbb63a7cb74a930e02ce3169fc2c756892
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 01:55:04 -0500

    add basic sidebar drawer

---
+++ src/modules/dashboard/components/Sidebar.tsx
@@ -0,0 +1,19 @@
+function Sidebar() {
+  return (
+    <div className="drawer-side">
+      <label
+        htmlFor="my-drawer-2"
+        aria-label="close sidebar"
+        className="drawer-overlay"></label>
+      <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <li>
+          <label htmlFor="my-drawer-2" className="cursor-pointer">
+            Home
+          </label>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
