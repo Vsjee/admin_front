5fd397ba96dd29083e9f2171232c6e0ea3f73970
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 00:49:43 -0500

    add daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 export default {
   content: [
     "./index.html",
@@ -28,6 +30,6 @@ export default {
       sans: ['Roboto', 'sans-serif']
     }
   },
-  plugins: [],
+  plugins: [  daisyui,],
 }
 
