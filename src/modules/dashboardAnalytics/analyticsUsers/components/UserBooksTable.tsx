c4152862696d081c1b7d5a0ff2381f799786d367
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-02 17:17:19 -0500

    add: books detail modal layout and edition

--- src/modules/dashboardAnalytics/analyticsUsers/components/UserBooksTable.tsx
+++ src/modules/dashboardAnalytics/analyticsUsers/components/UserBooksTable.tsx
@@ -193,6 +193,58 @@ interface PropsModal {
 }
 
 function BookInfoModal({ book }: PropsModal) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>('');
+
+  const toggleEditing = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleFormSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    updateBookById(title);
+  };
+
+  const updateBookById = (title: string) => {
+    if (book.story_title !== title) book.story_title = title;
+
+    booksService
+      .updateBookById(book)
+      .then((response) => {
+        console.log(response);
+        toast.success('Actualizado correctamente', {
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        });
+
+        toggleEditing();
+
+        setTimeout(() => {
+          window.location.reload();
+        }, 500);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error('Error al actualizar, intenta nuevamente.', {
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        });
+      });
+  };
+
   return (
     <>
       <ToastContainer />
@@ -200,7 +252,153 @@ function BookInfoModal({ book }: PropsModal) {
       <dialog
         id="modal_book_info"
         className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box"></div>
+        <div className="modal-box">
+          {!isEditing ? (
+            <>
+              <div className="flex justify-between">
+                <h3 className="font-bold text-lg">Informacion libro</h3>
+                <div
+                  className="swap-on btn btn-xs btn-outline btn-primary  text-white"
+                  onClick={toggleEditing}>
+                  Editar
+                </div>
+              </div>
+              <div className="py-4">
+                <div className="flex flex-col gap-3">
+                  {/* img */}
+                  <div className="card w-full h-40 bg-base-100 shadow-xl image-full">
+                    <figure>
+                      <img
+                        src={serverImgStgUrl + book.covers?.mini}
+                        alt={book.customer_id}
+                        style={{
+                          objectFit: 'cover',
+                          width: '100%',
+                          height: '100%',
+                        }}
+                      />
+                    </figure>
+                    <div className="card-body grid place-content-center">
+                      <h1 className="text-xl text-center">
+                        <br />
+                        <span className="text-4xl">
+                          {book.story_title ?? 'No registra nombre'}
+                        </span>
+                      </h1>
+                    </div>
+                  </div>
+
+                  {/* info */}
+                  <div className="flex flex-col gap-5 pt-5">
+                    <div>Id: {book._id}</div>
+                    <div>Titulo: {book.story_title}</div>
+                    <div className="w-full overflow-wrap break-word">
+                      Audio:{' '}
+                      {book.story_audio_path
+                        ? 'Posee audio'
+                        : 'No registra audio'}
+                    </div>
+                    <div>
+                      Este libro es de: {book.kid_name ?? 'No registar nombre'}
+                    </div>
+                    <div className=" h-60 overflow-scroll">
+                      Historia: {book.story_text}
+                    </div>
+
+                    <div>
+                      Fecha de creacion:{' '}
+                      {book.creation_date
+                        ? date_parser_util(book.creation_date)
+                        : 'Fecha no valida'}
+                    </div>
+                    <div>
+                      Fecha ultima actualizacion:{' '}
+                      {book.creation_date
+                        ? date_parser_util(book.modification_date!)
+                        : 'Fecha no valida'}
+                    </div>
+
+                    <div className="flex gap-3">
+                      <span
+                        className={
+                          book.is_active
+                            ? 'badge badge-outline badge-success btn-xs'
+                            : 'badge badge-outline badge-error btn-xs'
+                        }>
+                        {book.is_active ? 'Libro Activo' : 'Libro Inactivo'}
+                      </span>
+
+                      <span
+                        className={
+                          book.is_story_approved
+                            ? 'badge badge-outline badge-success btn-xs'
+                            : 'badge badge-outline badge-error btn-xs'
+                        }>
+                        {book.is_story_approved ? 'Aprobado' : 'Sin aprobar'}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button
+                    className="btn"
+                    onClick={() => {
+                      window.location.reload();
+                    }}>
+                    Cerrar
+                  </button>
+                </form>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex justify-between">
+                <h3 className="font-bold text-lg">Editando</h3>
+                <div
+                  className="swap-off btn btn-xs btn-outline btn-error  text-white"
+                  onClick={toggleEditing}>
+                  Cancelar
+                </div>
+              </div>
+              <div className="p-5 grid gap-5">
+                <form className="flex flex-col  gap-5">
+                  {/* name */}
+                  <label className="input input-bordered flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="w-4 h-4 opacity-70">
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input
+                      type="text"
+                      className="grow"
+                      placeholder="Titulo"
+                      value={title}
+                      onChange={(event) => setTitle(event.target.value)}
+                    />
+                  </label>
+                </form>
+
+                {title !== '' ? (
+                  <button
+                    className="btn btn-outline btn-primary"
+                    onClick={handleFormSubmit}>
+                    Actualizar
+                  </button>
+                ) : (
+                  <button className="btn btn-outline btn-primary" disabled>
+                    Actualizar
+                  </button>
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </dialog>
     </>
   );
