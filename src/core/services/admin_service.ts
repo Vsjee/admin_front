40ea188ec48972f85215ad332d366f5e24b04fa1
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-11 20:40:17 -0500

    update: admin service

--- src/core/services/admin_service.ts
+++ src/core/services/admin_service.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import { serverStgUrl } from '../../config/environment_stg_config';
-import { Admin } from '../types/admin_types';
+import { Admin, AdminUserProfile } from '../types/admin_types';
 
 class AdminService {
   // [GET]
@@ -13,6 +13,22 @@ class AdminService {
     axios.defaults.baseURL = `${serverStgUrl}/admin/${params}`;
 
     return axios.get<Admin>('');
+  }
+
+  // [GET] user by id
+  getAdminById(userId: string): Promise<AxiosResponse<AdminUserProfile>> {
+    axios.defaults.baseURL = `${serverStgUrl}/admin/id/${userId}`;
+
+    return axios.get<AdminUserProfile>('');
+  }
+
+  // [PATCH] update user password
+  updateAdminPassword(
+    user: AdminUserProfile
+  ): Promise<AxiosResponse<AdminUserProfile>> {
+    axios.defaults.baseURL = `${serverStgUrl}/admin/update`;
+
+    return axios.patch<AdminUserProfile>('', user);
   }
 }
 
--- src/core/types/admin_types.ts
+++ src/core/types/admin_types.ts
@@ -1,3 +1,13 @@
 export interface Admin {
   authorized: boolean;
+  userId: string;
+}
+
+export interface AdminUserProfile {
+  _id: string;
+  user: string;
+  password: string;
+  is_active: boolean;
+  is_admin: boolean;
+  modification_date: number;
 }
