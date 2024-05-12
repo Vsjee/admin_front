335d66c67e123039727c856f981ffe60e3763b3d
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-11 19:01:03 -0500

    remove

--- src/utils/store.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from '../core/interfaces';
-
-export const getCartTotalPrice = (cart: IProduct[]): string => {
-  let total = 0;
-  cart.map((item) => {
-    total += item.price * item.quantity;
-  });
-  return total.toLocaleString('es-CO');
-};
